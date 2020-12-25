840f89be6bc002fe4a8ff98c24c233eb952cf09b
Bryan <bryan.legras@gmail.com>
2020-12-25 18:55:25 +0100

    Adding TODO

--- index.php
+++ index.php
@@ -34,12 +34,7 @@ Flight::route('/', function(){
 
     $dinosaurs = getDinos();
     
-<<<<<<< HEAD
     echo 'TODO : ListeDinosaurs (views + datas)';
-=======
-    
->>>>>>> d824022e97f5b7e0c75f4430d434b36f7fbafc1a
-
 });
 
 
